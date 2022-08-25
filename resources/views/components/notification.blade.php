b044075960fdf07208aebd4567b37e6abd997e4d
Abel Morales <abel291m@gmail.com>
2022-08-25 04:57:12 -0500

    change text notification

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,9 +1,8 @@
 @if (session('success'))
-    <div
-        class="w-full bg-green-400 text-white p-2 md:p-3 flex items-start space-x-2 rounded-md">
+    <div class="w-full bg-green-400 text-white p-2 md:p-3 flex items-start space-x-2 rounded-md">
         <div class=" flex-grow">
             <span class="font-medium block ">
-                Registro agregado con exito
+                Usuario registrado con exito
             </span>
         </div>
     </div>
